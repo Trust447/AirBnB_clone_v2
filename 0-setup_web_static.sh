26fcd51696ef3144c9a50e23d7400f0e143968b8
trust <isaactrust447@gmail.com>
2024-02-02 01:16:57 +0100

    airbnb

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# sets up my web servers for the deployment of web_static
+
+# install nginx if not already installed
+if ! command -v nginx &> /dev/null; then
+	sudo apt-get -y update
+	sudo apt-get -y install nginx
+fi
+
+# check if folders already exist and create if non existent
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+
+# check if files exist and create if they don't exist
+touch /data/web_static/releases/test/index.html
+echo "<h1>Testing My website</h1>" > /data/web_static/releases/test/index.html
+
+# recreates a symbolic link and -f forces overwriting if another link exists
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# change ownership and group ownership to ubuntu and give privileges
+sudo chown -R ubuntu:ubuntu /data/
+sudo chmod -R 755 /data/
+# update the nginx configuration to serve content to hbnb_static
+sudo sed -i '48i \\t location /hbnb_static/ {\n\talias /data/web_static/current/;\n}\n' /etc/nginx/sites-available/default
+# restart nginx
+sudo service nginx restart
---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Its a python script that creates and distributes an
+archive to your web servers, using the function deploy
+"""
+
+from fabric.api import put, env, run
+import os
+
+env.hosts = ['18.209.225.36', '52.87.234.219']
+
+
+def do_deploy(archive_path):
+    """distributes files(archive) to my webservers"""
+
+    if os.path.exists(archive_path) is False:
+        return False
+    try:
+        put(archive_path, '/tmp/')
+        ext_file = archive_path.split("/")[-1]
+        filename = ext_file.split(".")[0]
+        run('mkdir -p /data/web_static/releases/{}'.format(filename))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format
+            (ext_file, filename))
+        run('rm /tmp/{}'.format(ext_file))
+        run('mv /data/web_static/releases/{}/web_static/* \
+                /data/web_static/releases/{}/'.format(filename, filename))
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(filename))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{} /data/web_static/current'
+            .format(filename))
