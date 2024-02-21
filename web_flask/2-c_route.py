24a529e791fe3fdde95b4407c90866f38b00d820
trust <isaactrust447@gmail.com>
2024-02-21 09:56:28 +0100

    route1

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""A script that starts a Flask web application"""
+"""A script that starts a Flask web application
+"""
 from flask import Flask
 
+app = Flask("__name__")
 
-app = Flask(__name__)
 
-
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def display():
     """return a string"""
-    return "Hello HBNB!"
+    return ("Hello HBNB!")
 
 
 @app.route("/hbnb", strict_slashes=False)
