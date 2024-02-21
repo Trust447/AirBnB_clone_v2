248568e9275f95d10e3ff9a7679bb68f1102fe44
trust <isaactrust447@gmail.com>
2024-02-21 10:02:29 +0100

    route1

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -15,14 +15,14 @@ def display():
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """return a HBNB"""
-    return "HBNB"
+    return ("HBNB")
 
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def display_text(text):
+def cText(text):
     """return a text"""
-    return "c {}".format(text.replace("_", " "))
+    return "C {}".format(text.replace("_", " "))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=None)
